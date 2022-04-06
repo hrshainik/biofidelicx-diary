ffa583f84654ba2565e0bea7c50d44e3b6866cd1
Habibur Rahman <hrsshainik@gmail.com>
2022-04-07 02:24:34 +0600

    postwidget design and fetching data form graphcms

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import moment from 'moment'
+import Link from 'next/link'
+import { getRecentPosts, getRelatedPosts } from '../services'
 
-const PostWidget = () => {
-  return <div>PostWidget</div>
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([])
+
+  useEffect(() => {
+    if (slug) {
+      getRelatedPosts(categories, slug).then((data) => setRelatedPosts(data))
+    } else {
+      getRecentPosts().then((data) => setRelatedPosts(data))
+    }
+  }, [slug])
+
+  return (
+    <div className="mb-8 rounded-lg bg-white p-8 shadow-lg">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+        {slug ? 'Related Posts' : 'Recent Posts'}
+      </h3>
+      {relatedPosts.map((post) => (
+        <div className="item-center mb-4 flex w-full" key={post.title}>
+          <div className="w-16 flex-none">
+            <img
+              src={post.featuredImage.url}
+              width="60px"
+              height="60px"
+              className="rounded-full align-middle"
+              alt={post.title}
+            />
+          </div>
+          <div className="ml-4 flex-grow">
+            <p className="font-xs text-gray-500">
+              {moment(post.createdAt).format('MMM DD, YYYY')}
+            </p>
+            <Link
+              href={`/post/${post.slug}`}
+              className="text-md"
+              key={post.title}
+            >
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default PostWidget
--- services/index.js
+++ services/index.js
@@ -37,3 +37,50 @@ export const getPosts = async () => {
 
   return result.postsConnection.edges
 }
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query)
+
+  return result.posts
+}
+
+export const getRelatedPosts = async () => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query)
+
+  return result.posts
+}
