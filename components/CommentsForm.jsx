086a10d82cfff5484307367ca5443826b79c2a6c
Habibur Rahman <hrsshainik@gmail.com>
2022-04-18 12:25:35 +0600

    sending comment to cms

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,7 +1,150 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import { submitComment } from '../services'
 
-const CommentsForm = () => {
-  return <div>CommentsForm</div>
+const CommentsForm = ({ slug }) => {
+  const [error, setError] = useState(false)
+  const [localStorage, setLocalStorage] = useState(null)
+  const [showSuccessMsg, setShowSuccessMsg] = useState(false)
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    comment: '',
+    storeData: false,
+  })
+
+  useEffect(() => {
+    setLocalStorage(window.localStorage)
+    const initalFormData = {
+      name: window.localStorage.getItem('name'),
+      email: window.localStorage.getItem('email'),
+      storeData:
+        window.localStorage.getItem('name') ||
+        window.localStorage.getItem('email'),
+    }
+    setFormData(initalFormData)
+  }, [])
+
+  const onInputChange = (e) => {
+    const { target } = e
+    if (target.type === 'checkbox') {
+      setFormData((prevState) => ({
+        ...prevState,
+        [target.name]: target.checked,
+      }))
+    } else {
+      setFormData((prevState) => ({
+        ...prevState,
+        [target.name]: target.value,
+      }))
+    }
+  }
+
+  const handleCommentSubmission = () => {
+    setError(false)
+
+    const { name, email, comment, storeData } = formData
+
+    if (!comment || !name || !email) {
+      setError(true)
+      return
+    }
+
+    const commentObj = {
+      name,
+      email,
+      comment,
+      slug,
+    }
+
+    if (storeData) {
+      localStorage.setItem('name', name)
+      localStorage.setItem('email', email)
+    } else {
+      localStorage.removeItem('name', name)
+      localStorage.removeItem('email', email)
+    }
+
+    submitComment(commentObj).then((res) => {
+      setShowSuccessMsg(true)
+
+      setTimeout(() => {
+        setShowSuccessMsg(false)
+      }, 3000)
+    })
+  }
+
+  return (
+    <div className="mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+        Leave a Reply
+      </h3>
+      <div className="mb-4 grid grid-cols-1 gap-4">
+        <textarea
+          name="comment"
+          className="w-full rounded-lg bg-gray-100 p-4 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
+          placeholder="Comment"
+          value={formData.comment}
+          onChange={onInputChange}
+        />
+      </div>
+      <div className="mb-4 grid grid-cols-1 gap-4 lg:grid-cols-2">
+        <input
+          type="text"
+          // ref={nameEl}
+          value={formData.name}
+          onChange={onInputChange}
+          className="w-full rounded-lg bg-gray-100 py-2 px-4 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          type="text"
+          // ref={emailEl}
+          value={formData.email}
+          onChange={onInputChange}
+          className="w-full rounded-lg bg-gray-100 py-2 px-4 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
+          placeholder="Email"
+          name="email"
+        />
+      </div>
+      <div className="mb-4 grid grid-cols-1 gap-4">
+        <div>
+          <input
+            type="checkbox"
+            // ref={storeDataEl}
+            checked={formData.storeData}
+            onChange={onInputChange}
+            id="storeData"
+            name="storeData"
+            value="true"
+          />
+          <label
+            className="ml-2 cursor-pointer text-gray-500"
+            htmlFor="storeData"
+          >
+            Save my email for the next time I comment
+          </label>
+        </div>
+      </div>
+      {error && (
+        <p className="text-xs text-red-500">All fields are required.</p>
+      )}
+      <div className="mt-8">
+        <button
+          type="button"
+          onClick={handleCommentSubmission}
+          className="ease inline-block cursor-pointer rounded-full bg-pink-600 px-8 py-3 text-lg text-white transition duration-500 hover:bg-indigo-900"
+        >
+          Post Comment
+        </button>
+        {showSuccessMsg && (
+          <span className="float-right mt-3 text-xl font-semibold text-green-500">
+            Comment submitted for review
+          </span>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default CommentsForm
--- services/index.js
+++ services/index.js
@@ -134,3 +134,15 @@ export const getCategories = async () => {
 
   return result.categories
 }
+
+export const submitComment = async (obj) => {
+  const result = await fetch('/api/comments', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(obj),
+  })
+
+  return result.json()
+}
