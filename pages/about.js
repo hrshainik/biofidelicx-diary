7501bb340bb9a4fc0f00ed6a70d123db91a0ef2c
Habibur Rahman <hrsshainik@gmail.com>
2022-11-16 00:21:41 +0600

    add about and contact blank page

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>About - biofidelicX</title>
+      </Head>
+      <h1>About</h1>
+    </>
+  )
+}
+
+export default About
---
+++ pages/contact.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <>
+      <Head>
+        <title>Contact - biofidelicX</title>
+      </Head>
+      <h1>Contact</h1>
+    </>
+  )
+}
+
+export default Contact
