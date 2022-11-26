58501b3d9d0971b48dcafb4b9e75b8d781276396
Habibur Rahman <hrsshainik@gmail.com>
2022-11-27 02:02:01 +0600

    add next js redirects with manual redirects

--- next.config.js
+++ next.config.js
@@ -5,4 +5,13 @@ module.exports = {
     domains: ['media.graphassets.com', 'media.graphcms.com'],
     formats: ['image/webp'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/post-page/1',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
 }
